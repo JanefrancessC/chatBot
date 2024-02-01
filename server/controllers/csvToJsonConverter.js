b8863b1adcda4de6ecd708fc0fb753663a5ab920
JanefrancessC <janefrancesschioma@gmail.com>
2024-02-01 12:17:16 +0000

    removed commented codes

--- server/controllers/csvToJsonConverter.js
+++ server/controllers/csvToJsonConverter.js
@@ -4,27 +4,9 @@ import { dirname } from "node:path";
 
 const currentFilePath = fileURLToPath(import.meta.url);
 const currentDir = dirname(currentFilePath);
-// console.log("currentDir: ", currentDir);
 
 const csvFilePath = pathToFileURL(`${currentDir}/../data/leads.csv`);
 
-
-// const csvFilePath = path.resolve(__dirname, "../data/leads.csv");
-
-// This function converts CSV files to JSON format
-// export const convertCSVToJSON = async () => {
-//   try {
-//     const jsonData = await csvToJSON({
-//       filePath: csvFilePath,
-//       separator: ",",
-//     });
-//     console.log("Converted data: ", jsonData);
-
-//     return jsonData;
-//   } catch (error) {
-//     console.error({ "Error locating file": error.message });
-//   }
-// };
 
 export const convertCSVToJSON = async () => {
   return new Promise(async (resolve, reject) => {
@@ -33,7 +15,6 @@ export const convertCSVToJSON = async () => {
         filePath: csvFilePath,
         separator: ",",
       });
-      // console.log("Converted data: ", jsonData);
       resolve(jsonData);
     } catch (error) {
       console.error({ "Error locating file": error.message });
